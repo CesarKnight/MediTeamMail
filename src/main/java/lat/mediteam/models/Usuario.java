a2c87fa516bd77c7119a3a3818a42830e9020566
CesarLinux <udemicscesar@gmail.com>
2026-05-15 21:58:15 -0400

    cambiar modo de id en usuario a ID

--- src/main/java/lat/mediteam/models/Usuario.java
+++ src/main/java/lat/mediteam/models/Usuario.java
@@ -15,7 +15,7 @@ import lat.mediteam.enums.UsuarioTipo;
 @Entity
 public class Usuario {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false, unique = true)
