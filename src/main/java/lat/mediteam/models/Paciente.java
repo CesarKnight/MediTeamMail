d7d22a6ba8b131d9b3c558c4349cd895e9ef158b
CesarLinux <udemicscesar@gmail.com>
2026-05-23 21:59:34 -0400

    Revertir "feat: agregar casos de uso medico, historia clinica, diagnostico, tratamiento e involucrados"
    This reverts commit 8e502c7918dd0c73abeb55a9cb890a8eeacc2fc5.

--- src/main/java/lat/mediteam/models/Paciente.java
+++ src/main/java/lat/mediteam/models/Paciente.java
@@ -7,10 +7,8 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.MapsId;
 import jakarta.persistence.OneToOne;
-import lombok.Getter;
 
 @Entity
-@Getter
 public class Paciente {
     @Id
     private Long id;
